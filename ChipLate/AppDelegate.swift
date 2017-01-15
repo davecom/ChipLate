85d434f84fa49f3e9b871052f296bf2de4c9f895
Dave <snpukwana@gmail.com>
2017-01-15 02:18:52 -0500

    Initial Commit

---
+++ ChipLate/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ChipLate
+//
+//  Created by David Kopec on 1/15/17.
+//  Copyright © 2017 David Kopec. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
